ecf0c1e2c621c1f4fd09c39a1a7111373f1db306
༺☆Ｙศ𝗺ᾰт𝗈ę𝔫𝘥𝚘☆༻ <developerbeta338@gmail.com>
2025-08-15 11:03:21 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -2,9 +2,9 @@
 module.exports = {
   SESSION_ID: "",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "237676324978", // put your phone number here
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  PREFIX: " ", // prefix (e.g., ., /, !, *)
   
   TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
 };
